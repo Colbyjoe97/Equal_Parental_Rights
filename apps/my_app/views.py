886ed69d51960720bd109185e461103f65353195
colby <dumonchelle123@gmail.com>
2021-12-12 16:38:54 -0500

    Added form to sign bill

--- apps/my_app/views.py
+++ apps/my_app/views.py
@@ -5,15 +5,24 @@ from django.contrib import messages
 import bcrypt
 # Create your views here.
 def index(request):
-    # users = User.objects.all()
-    # for user in users:
-    #     user.delete()
+    us_states = [("AL","Alabama"),("AK","Alaska"),("AZ","Arizona"),("AR","Arkansas"),("CA", "California"),("CO", "Colorado"),
+    ("CT","Connecticut"),("DC","Washington DC"),("DE","Delaware"),("FL","Florida"),("GA","Georgia"),
+    ("HI","Hawaii"),("ID","Idaho"),("IL","Illinois"),("IN","Indiana"),("IA","Iowa"),("KS","Kansas"),("KY","Kentucky"),
+    ("LA","Louisiana"),("ME","Maine"),("MD","Maryland"),("MA","Massachusetts"),("MI","Michigan"),("MN","Minnesota"),
+    ("MS","Mississippi"),("MO","Missouri"),("MT","Montana"),("NE","Nebraska"),("NV","Nevada"),("NH","New Hampshire"),
+    ("NJ","New Jersey"),("NM","New Mexico"),("NY","New York"),("NC","North Carolina"),("ND","North Dakota"),("OH","Ohio"),
+    ("OK","Oklahoma"),("OR","Oregon"),("PA","Pennsylvania"),("RI","Rhode Island"),("SC","South Carolina"),("SD","South Dakota"),
+    ("TN","Tennessee"),("TX","Texas"),("UT","Utah"),("VT","Vermont"),("VA","Virginia"),("WA","Washington"),("WV","West Virginia"),
+    ("WI","Wisconsin"),("WY","Wyoming")]
     if 'user' in request.session:
         context = {
-            'user': User.objects.get(id=request.session['user'])
+            'user': User.objects.get(id=request.session['user']),
+            'states': us_states
         }
     else:
-        context = {}
+        context = {
+            'states': us_states
+        }
     return render(request, 'index.html', context)
 
 
