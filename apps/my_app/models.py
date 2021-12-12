ace75598ed3773aa5570b8e922838bbc993fc65d
colby <dumonchelle123@gmail.com>
2021-12-12 15:07:11 -0500

    created user model

--- apps/my_app/models.py
+++ apps/my_app/models.py
@@ -1,3 +1,68 @@
 from django.db import models
-
+import re, bcrypt
 # Create your models here.
+class UserManager(models.Manager):
+    def registrationValidator(self, postData):
+        matchingEmail = User.objects.filter(email=postData['email'])
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        errors = {}
+
+        if len(postData['fname']) == 0:
+            errors['fname'] = "First name is required"
+        elif len(postData['fname']) < 3:
+            errors['fname'] = "First name must be at least 2 characters long"
+
+        if len(postData['lname']) == 0:
+            errors['lname'] = "Last name is required"
+        elif len(postData['lname']) < 3:
+            errors['lname'] = "Last name must be at least 2 characters long"
+
+        if len(postData['email']) == 0:
+            errors['email'] = "Email is required"
+        elif not EMAIL_REGEX.match(postData['email']):
+            errors['email'] = "Email not in correct format"
+
+
+        if len(postData['pass']) == 0:
+            errors['pass'] = "Password is required"
+        elif len(postData['pass']) < 8:
+            errors['pass'] = "Password must be at least 8 characters"
+        elif postData['pass'] != postData['confirmPass']:
+            errors['pass'] = "Passwords do not match"
+
+        return errors
+
+    def loginValidator(self, postData):
+        matchingEmail = User.objects.filter(email=postData['email'])
+        errors = {}
+
+        if len(postData['email']) == 0:
+            errors['email'] = "Email is required"
+        elif len(matchingEmail) == 0:
+            errors['email'] = "Email is not yet registered"
+
+        if len(postData['pass']) == 0:
+            errors['pass'] = "Password is required"
+        elif not bcrypt.checkpw(postData['pass'].encode(), matchingEmail[0].password.encode()):
+            errors['pass'] = "Password is incorrect"
+
+        return errors
+
+class Signature(models.Model):
+    first_name=models.CharField(max_length=255)
+    last_name=models.CharField(max_length=255)
+    state=models.CharField(max_length=255)
+    age=models.IntegerField()
+    sex=models.CharField(max_length=255)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+
+class User(models.Model):
+    first_name=models.CharField(max_length=255)
+    last_name=models.CharField(max_length=255)
+    email=models.CharField(max_length=255)
+    password=models.CharField(max_length=255)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    objects=UserManager()
