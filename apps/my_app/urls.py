385c22b44638381e87caf7afe09d7d2268fca7ae
colbyjoe97 <dumonchelle123@gmail.com>
2021-12-14 18:09:47 -0500

    added route for login

--- apps/my_app/urls.py
+++ apps/my_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^sign$', views.sign),
     url(r'^admin/register$', views.admin),
     url(r'^register$', views.register_admin),
+    url(r'^login$', views.login),
     url(r'^admin/page$', views.admin_page),
 ]
