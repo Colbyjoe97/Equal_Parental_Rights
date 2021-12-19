8fdf3d0fef2863aff286ad897653b5247a5b4509
colbyjoe97 <dumonchelle123@gmail.com>
2021-12-19 15:27:58 -0500

    added catch-all route and gave names to existing routes

--- apps/my_app/urls.py
+++ apps/my_app/urls.py
@@ -1,11 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import url, handler404
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^sign$', views.sign),
+    url(r'^sign$', views.sign, name="sign_form"),
     url(r'^admin/register$', views.admin),
-    url(r'^register$', views.register_admin),
-    url(r'^login$', views.login),
-    url(r'^admin/page$', views.admin_page),
+    url(r'^register$', views.register_admin, name="register_admin"),
+    url(r'^login$', views.login, name="login"),
+    url(r'^admin/page$', views.admin_page, name="admin_page"),
+    url(r'^', views.bad_request),
 ]
