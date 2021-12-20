52d07bd38eb6d75de263e1fb38a1c8cbbbf4651c
colbyjoe97 <dumonchelle123@gmail.com>
2021-12-20 16:26:49 -0500

    removed test

--- Equal_Parental_Rights/settings.py
+++ Equal_Parental_Rights/settings.py
@@ -127,10 +127,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-if os.getcwd() == '/app':
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    SECURE_SSL_REDIRECT = True
-    DEBUG = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
